8ccd005e478bc73eac3f67fa6869d6ad46bab773
ayhan5 <hey956@gmail.com>
2021-04-10 11:40:17 -0400

    2.commit

--- src/firstGitProject/HelloJava.java
+++ src/firstGitProject/HelloJava.java
@@ -5,6 +5,8 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("04/10/2021 1.commit");
+		System.out.println("04/10/2021 2.commit");
+		
 	}
 
 }
